3941fc5b551f9423ebd08dc85679232e8dd3f142
Oliver Siegmar <oliver@siegmar.de>
2021-02-14 19:38:25 +0100

    initial commit

---
+++ impl/fastcsv/entrypoint.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+/app/build/install/fastcsv-cli/bin/fastcsv-cli "$@"
---
+++ impl/fastcsv/src/main/java/de/siegmar/fastcsv/cli/FastCsv.java
@@ -0,0 +1,9 @@
+package de.siegmar.fastcsv.cli;
+
+public class FastCsv {
+
+    public static void main(String[] args) {
+        System.out.println("Hello " + args[0]);
+    }
+
+}
